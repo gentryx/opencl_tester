dc6d24bbcbd85a7c25a2eb57116677ae97627ae4
Andreas Schäfer <gentryx@gmx.de>
2013-06-28 14:25:01 +0200

    devices are now being tested with a simple kernel

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,81 @@ void rot13 (char *buf)
             buf[index] = buf[index];
         } else {
             if (c>'m' || (c>'M' && c<'a')) {
-	        buf[index] = buf[index]-13;
-	    } else {
-	        buf[index] = buf[index]+13;
-	    }
+                buf[index] = buf[index]-13;
+            } else {
+                buf[index] = buf[index]+13;
+            }
         }
-	c=buf[++index];
+        c=buf[++index];
     }
+}
+
+bool evaluateDevice(cl::Device device)
+{
+    std::vector<cl::Device> devices(1, device);
+    cl::Context context = cl::Context(devices);
+    cl::CommandQueue cmdQueue = cl::CommandQueue(context, device);
+
+    std::string clSourceString = "                                      \
+__kernel void rot13                                                     \
+    (   __global    const   char*    in                                 \
+    ,   __global            char*    out                                \
+    )                                                                   \
+{                                                                       \
+    const uint index = get_global_id(0);                                \
+                                                                        \
+    char c=in[index];                                                   \
+    if (c<'A' || c>'z' || (c>'Z' && c<'a')) {                           \
+        out[index] = in[index];                                         \
+    } else {                                                            \
+        if (c>'m' || (c>'M' && c<'a')) {                                \
+            out[index] = in[index]-13;                                  \
+        } else {                                                        \
+            out[index] = in[index]+13;                                  \
+        }                                                               \
+    }                                                                   \
+}                                                                       \
+";
+
+    cl::Program::Sources clSource(
+        1,
+        std::make_pair(clSourceString.c_str(),
+                       clSourceString.size()));
+    cl::Program clProgram(context, clSource);
+
+    try {
+        clProgram.build(devices);
+    } catch (...) {
+        // Normally we don't catch exceptions, but in this case
+        // printing the build log (which might get lost otherwise)
+        // is valuable for the user who needs to debug his code.
+        std::cerr << "Build Log: "
+                  << clProgram.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << "\n";
+        throw;
+    }
+
+    cl::Kernel kernel = cl::Kernel(clProgram, "rot13");
+
+    std::string message = "blahblahblah";
+    rot13(const_cast<char*>(message.c_str()));
+    std::string result = message;
+
+    size_t worksize = message.length();
+    cl::NDRange globalRange(worksize, 1, 1);
+    cl::NDRange localRange(1, 1, 1);
+
+    cl::Buffer bufferIn  = cl::Buffer(context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY,  worksize, &message[0]);
+    cl::Buffer bufferOut = cl::Buffer(context, CL_MEM_COPY_HOST_PTR | CL_MEM_WRITE_ONLY, worksize, &result[0]);
+
+    cl::KernelFunctor kernelFunctor = kernel.bind(cmdQueue, globalRange, localRange);
+    kernelFunctor(bufferIn, bufferOut);
+
+    cmdQueue.enqueueReadBuffer(
+        bufferOut, true, 0, worksize, &result[0]);
+
+    cmdQueue.finish();
+
+    return (kernelFunctor.getError() == 0) && (result == "blahblahblah");
 }
 
 void evaluatePlatform(cl::Platform platform)
@@ -67,10 +135,11 @@ void evaluatePlatform(cl::Platform platform)
         }
 
         std::cout << "    - device[" << j << "]\n"
-                  << "      - name:  " << deviceName << "\n"
-                  << "      - type:  " << typeString << "\n"
-                  << "      - units: " << deviceMaxComputeUnits << "\n"
-                  << "      - mem:   " << deviceGlobalMemSize << "\n";
+                  << "      - name:   " << deviceName << "\n"
+                  << "      - type:   " << typeString << "\n"
+                  << "      - units:  " << deviceMaxComputeUnits << "\n"
+                  << "      - mem:    " << deviceGlobalMemSize << "\n"
+                  << "      - status: " << (evaluateDevice(device) ? "OK" : "broken") << "\n";
     }
 }
 
