845729a24ee5d7a9a0be9daf4c035fae06673152
Andreas Schäfer <gentryx@gmx.de>
2013-06-28 14:10:57 +0200

    can now list all devices

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #define __CL_ENABLE_EXCEPTIONS
 #endif
 
+std::set<std::string> blacklist;
+
 void rot13 (char *buf)
 {
     int index=0;
@@ -22,6 +24,82 @@ void rot13 (char *buf)
 	    }
         }
 	c=buf[++index];
+    }
+}
+
+void evaluatePlatform(cl::Platform platform)
+{
+    std::string platformName;
+    platform.getInfo(CL_PLATFORM_NAME, &platformName);
+
+    if (blacklist.count(platformName)) {
+        return;
+    }
+
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    for (int j = 0; j < devices.size(); ++j) {
+        cl_device_type deviceType;
+        cl_uint deviceMaxComputeUnits;
+        std::string deviceName;
+        cl_ulong deviceGlobalMemSize;
+
+        cl::Device& device = devices[j];
+        device.getInfo(CL_DEVICE_TYPE,              &deviceType);
+        device.getInfo(CL_DEVICE_MAX_COMPUTE_UNITS, &deviceMaxComputeUnits);
+        device.getInfo(CL_DEVICE_NAME,              &deviceName);
+        device.getInfo(CL_DEVICE_GLOBAL_MEM_SIZE,   &deviceGlobalMemSize);
+
+        if (blacklist.count(deviceName)) {
+            continue;
+        }
+
+        std::string typeString = "";
+        if (deviceType & CL_DEVICE_TYPE_CPU) {
+            typeString += "CPU ";
+        }
+        if (deviceType & CL_DEVICE_TYPE_GPU) {
+            typeString += "GPU ";
+        }
+        if (deviceType & CL_DEVICE_TYPE_ACCELERATOR) {
+            typeString += "ACCELERATOR ";
+        }
+
+        std::cout << "    - device[" << j << "]\n"
+                  << "      - name:  " << deviceName << "\n"
+                  << "      - type:  " << typeString << "\n"
+                  << "      - units: " << deviceMaxComputeUnits << "\n"
+                  << "      - mem:   " << deviceGlobalMemSize << "\n";
+    }
+}
+
+void listPlatforms()
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    std::cout << "got " << platforms.size() << " platforms\n\n";
+
+    for (int i = 0; i < platforms.size(); ++i) {
+        std::string platformName, platformProfile, platformVersion, platformVendor, platformExtensions;
+        cl::Platform& platform = platforms[i];
+
+        platform.getInfo(CL_PLATFORM_NAME,    &platformName);
+        platform.getInfo(CL_PLATFORM_PROFILE, &platformProfile);
+        platform.getInfo(CL_PLATFORM_VERSION, &platformVersion);
+        platform.getInfo(CL_PLATFORM_VENDOR,  &platformVendor);
+
+        std::vector<cl::Device> devices;
+        platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+        std::cout << "- platform[" << i << "]\n"
+                  << "  - name:    " << platformName    << "\n"
+                  << "  - profile: " << platformProfile << "\n"
+                  << "  - version: " << platformVersion << "\n"
+                  << "  - vendor:  " << platformVendor  << "\n"
+                  << "  - devices: " << devices.size()  << "\n";
+
+        evaluatePlatform(platform);
     }
 }
 
@@ -46,8 +124,6 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    std::set<std::string> blacklist;
-
     if (varMap.count("blacklist")) {
         std::vector<std::string> entries = varMap["blacklist"].as<std::vector<std::string> >();
         for (int i = 0; i < entries.size(); ++i) {
@@ -56,67 +132,7 @@ int main(int argc, char **argv)
         }
     }
 
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
-    std::cout << "got " << platforms.size() << " platforms\n\n";
-
-    for (int i = 0; i < platforms.size(); ++i) {
-        std::string platformName, platformProfile, platformVersion, platformVendor, platformExtensions;
-        cl::Platform& platform = platforms[i];
-
-        platform.getInfo(CL_PLATFORM_NAME,    &platformName);
-        platform.getInfo(CL_PLATFORM_PROFILE, &platformProfile);
-        platform.getInfo(CL_PLATFORM_VERSION, &platformVersion);
-        platform.getInfo(CL_PLATFORM_VENDOR,  &platformVendor);
-
-        std::vector<cl::Device> devices;
-        platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-
-        std::cout << "- platform[" << i << "]\n"
-                  << "  - name:    " << platformName    << "\n"
-                  << "  - profile: " << platformProfile << "\n"
-                  << "  - version: " << platformVersion << "\n"
-                  << "  - vendor:  " << platformVendor  << "\n"
-                  << "  - devices: " << devices.size()  << "\n";
-
-        if (blacklist.count(platformName)) {
-            continue;
-        }
-
-        for (int j = 0; j < devices.size(); ++j) {
-            cl_device_type deviceType;
-            cl_uint deviceMaxComputeUnits;
-            std::string deviceName;
-            cl_ulong deviceGlobalMemSize;
-
-            cl::Device& device = devices[i];
-            device.getInfo(CL_DEVICE_TYPE,              &deviceType);
-            device.getInfo(CL_DEVICE_MAX_COMPUTE_UNITS, &deviceMaxComputeUnits);
-            device.getInfo(CL_DEVICE_NAME,              &deviceName);
-            device.getInfo(CL_DEVICE_GLOBAL_MEM_SIZE,   &deviceGlobalMemSize);
-
-            if (blacklist.count(deviceName)) {
-                continue;
-            }
-
-            std::string typeString = "";
-            if (deviceType & CL_DEVICE_TYPE_CPU) {
-                typeString += "CPU ";
-            }
-            if (deviceType & CL_DEVICE_TYPE_GPU) {
-                typeString += "GPU ";
-            }
-            if (deviceType & CL_DEVICE_TYPE_ACCELERATOR) {
-                typeString += "ACCELERATOR ";
-            }
-
-            std::cout << "    - device[" << j << "]\n"
-                      << "      - name:  " << deviceName << "\n"
-                      << "      - type:  " << typeString << "\n"
-                      << "      - units: " << deviceMaxComputeUnits << "\n"
-                      << "      - mem:   " << deviceGlobalMemSize << "\n";
-        }
-    }
+    listPlatforms();
 
     return 0;
 }
